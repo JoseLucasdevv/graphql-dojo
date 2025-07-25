bc939872baaefce86bfc7e5337992eca1b277015
JoseLucasdevv <joselucas.dev@gmail.com>
2025-07-25 10:20:59 -0300

    lint: remove useless imports.

--- src/main/java/com/lucas/back/end/java/graphql/api/controller/UserController.java
+++ src/main/java/com/lucas/back/end/java/graphql/api/controller/UserController.java
@@ -5,15 +5,9 @@ import com.lucas.back.end.java.graphql.api.page.ContentPageable;
 import com.lucas.back.end.java.graphql.api.service.UserService;
 
 import com.lucas.back.end.java.graphql.api.entity.User;
-import jakarta.annotation.Nullable;
-import org.springframework.data.domain.Limit;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.ScrollPosition;
-import org.springframework.data.domain.Window;
 import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.MutationMapping;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
-import org.springframework.graphql.data.query.ScrollSubrange;
 import org.springframework.stereotype.Controller;
 
 
